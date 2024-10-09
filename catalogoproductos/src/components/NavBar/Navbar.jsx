25fe92681e1b43a3ad9ec4c1c33a1c082797b6ce
maxisher8 <msher2006@gmail.com>
2024-10-09 18:22:48 -0300

    9/10 pagina contacto

--- catalogoproductos/src/components/NavBar/Navbar.jsx
+++ catalogoproductos/src/components/NavBar/Navbar.jsx
@@ -9,6 +9,7 @@ export default function Navbar() {
         <div className={styles.navbar}>
             <button type="button" onClick={() => router.push('/')}> Home </button>
             <button type="button" onClick={() => router.push('/Productos')}> Productos </button>
+            <button type="button" onClick={() => router.push('/Contacto')}> Contacto </button>
         </div>
     );
   }  
