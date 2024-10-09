47797637fb36ef4a39aa170b5cfa69dd7ff27e37
maxisher8 <msher2006@gmail.com>
2024-10-09 19:09:50 -0300

    imagenes reales

--- catalogoproductos/src/products.js
+++ catalogoproductos/src/products.js
@@ -1,39 +1,39 @@
 const products = [
     {
-        name: "Camiseta",
-        desc: "Camiseta de algodón de alta calidad, cómoda y ligera.",
-        image: "https://via.placeholder.com/150?text=Camiseta",
+        name: "Camiseta LeBron x Liverpool FC",
+        desc: "Celebra la asociación entre LeBron James y el Liverpool FC con este camiseta DNA. Al traspasar los límites entre el fútbol y el básquetbol, este estilo de ajuste relajado es ligero y transpirable para ayudar a mantener la frescura en la cancha, el campo y más allá.",
+        image: "https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw90ad409a/products/NIFZ3199-011/NIFZ3199-011-1.JPG",
         cat: "torso"
     },
     {
-        name: "Jeans",
-        desc: "Jeans ajustados, perfectos para cualquier ocasión.",
-        image: "https://via.placeholder.com/150?text=Jeans",
-        cat: "pantalones"
-    },
-    {
-        name: "Chaqueta",
-        desc: "Chaqueta de cuero, ideal para el clima fresco.",
-        image: "https://via.placeholder.com/150?text=Chaqueta",
+        name: "Camiseta Kevin Durant PHX Suns City Edition",
+        desc: "la camiseta City Edition 2023/24 de los Suns rinde homenaje a la cultura mexicana arraigada en El Valle. Con colores vivos y detalles que recuerdan a los lowriders clásicos, este camiseta rinde homenaje al icónico arte chicano de andar bajo y despacio.",
+        image: "https://basketworld.com/26173-home_default/kevin-durant-phoenix-suns-statement-edition-swingman-jersey.jpg",
         cat: "torso"
     },
     {
-        name: "Vestido",
-        desc: "Vestido elegante para ocasiones especiales.",
-        image: "https://via.placeholder.com/150?text=Vestido",
+        name: "Campera San Lorenzo 2024",
+        desc: "La campera de entrenamiento Nike Dri-FIT Strike cuenta con detalles diseñados específicamente para las estrellas del fútbol en ascenso. Con un ajuste fino y aerodinámico, nada se interpone entre vos y la pelota, mientras que el tejido a prueba de transpiración te mantiene fresco para que brillen tus habilidades.",
+        image: "https://afaar.vtexassets.com/arquivos/ids/156729-800-auto?v=638521696124770000&width=800&height=auto&aspect=true",
         cat: "torso"
     },
     {
-        name: "Zapatillas",
-        desc: "Zapatillas deportivas, cómodas para el día a día.",
-        image: "https://via.placeholder.com/150?text=Zapatillas",
+        name: "Top Nike Pro Swoosh",
+        desc: "Cuando te esfuerzas, la tela ligera y adaptable del bra se siente como una segunda piel y brinda una comodidad superior. Cuando es momento de descansar, recupera su forma rápidamente y brinda un ajuste ceñido. Las fibras superelásticas facilitan la experiencia de ponerse y quitarse el bra para que no tengas que doblarte hacia atrás para quitártelo.",
+        image: "https://www.stockcenter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dwd7fb4e4b/products/NIDX6821-010/NIDX6821-010-1.JPG",
+        cat: "torso"
+    },
+    {
+        name: "Zapatillas Nike Dunk Low Retro Premium",
+        desc: "El ícono del básquetbol de los años 80 se creó para la cancha, pero conquistó las calles. Ahora vuelve con detalles clásicos y el resplandor del básquetbol de antaño. Canalizando el estilo vintage de regreso a las calles, su cuello acolchado de corte low te permite llevar tu juego a cualquier parte, con comodidad.",
+        image: "https://www.digitalsport.com.ar/files/products/665df61832d91-615172-1200x1200.jpg",
         cat: "calzado"
     },
     {
-        name: "Bufanda",
-        desc: "Bufanda de lana, perfecta para mantenerte abrigado.",
-        image: "https://via.placeholder.com/150?text=Bufanda",
-        cat: "torso"
+        name: "Pantalon Nike Sportswear Phoenix Fleece",
+        desc: "Con estilo, comodidad y versatilidad, descubre nuestra ropa de lujo para estar en casa. Con un diseño de pierna ancha y un largo cropped, estos joggers hacen que tu aspecto luzca cualquier cosa menos básico, mientras que el tejido Fleece cepillado de densidad media te mantiene cómoda durante todo el día.",
+        image: "https://www.tripstore.com.ar/media/catalog/product/cache/4769e4d9f3516e60f2b4303f8e5014a8/D/Q/DQ5615-010_0_1.jpg",
+        cat: "pantalon"
     }
 ];
 
